f0edc6fc23e3863fcf0d39dfb0ef8fa42a53bd2f
Stephanie <stephanie.enciso13@gmail.com>
2021-02-11 00:15:00 -0600

    Finished all of MVP.

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,45 +7,93 @@ const {
 } = require('../middleware/middleware');
 
 const User = require('./users-model');
+const Post = require('../posts/posts-model');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
-
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'The users could not be retrieved.'
+      });
+    });
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
   res.status(200).json(req.user);
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post('/', validateUser, (req, res) => {
+  User.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'The user could not be added.'
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  User.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'User could not be updated'
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+router.delete('/:id', validateUserId, (req, res) => {
+  User.remove(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'User could not be removed.'
+      })
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+router.get('/:id/posts', validateUserId, (req, res) => {
+  User.getUserPosts(req.params.id)
+  .then(userPosts => {
+    res.status(200).json(userPosts);
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: "The user's posts could not be retrieved."
+    });
+  });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  const postInfo = {...req.body, user_id: req.params.id}
+  Post.insert(postInfo)
+    .then(post => {
+      res.status(201).json(postInfo);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Post could not be added.'
+      })
+    })
 });
 
 // do not forget to export the router
